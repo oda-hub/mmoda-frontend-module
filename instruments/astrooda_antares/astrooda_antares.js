58c357cd383929483dfcc02346f8d0180c75f7a0
Denys Savchenko <speleoden@gmail.com>
2021-05-12 09:46:59 +0200

    try 2 antares roi validation

--- instruments/astrooda_antares/astrooda_antares.js
+++ instruments/astrooda_antares/astrooda_antares.js
@@ -1,33 +1,37 @@
-(function($) {
+var avalidator = $('.antares-form').bootstrapValidator({
+    // live :'disabled',
+    fields: {
+      'radius': {
+        // enabled: false,
+        validators: {
+          callback: {
+            callback: function(value, avalidator, $field) {
+              var arad = avalidator.getFieldElements('radius').val();
+              if ( Number(arad) < 0.1 || Number(arad) > 2.5 ) {
+                return {
+                  valid: false,
+                  message: 'Please enter value between 0.1 and 2.5 deg'
+                }
+              }
+              return true;
+            }
+          }
+        }
+      },
 
-	$(document).ready(
-			function() {
-				$('.instrument-panel-antares form').bootstrapValidator({
-						fields : {
-							// live :'disabled',
-							'radius' : {
-								// enabled: false,
-								validators : {
-									callback : {
-										callback : function(value, validator,
-												$field) {
-											if (value < 0.1 || value > 2.5) {
-                                                return {
-                                                    valid: false,
-                                                    message: "Please enter value between 0.1 and 2.5 deg"
-                                                }
-                                            } 
-                                            return true;
-										}
-									}
-								}
-							}
-						},
-						feedbackIcons : {
-							valid : 'glyphicon glyphicon-ok',
-							invalid : 'glyphicon glyphicon-remove',
-							validating : 'glyphicon glyphicon-refresh'
-						}
-			}).data('bootstrapValidator');
-});
-})(jQuery);
+    },
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    }
+  }).data('bootstrapValidator');// .validate();
+
+  // if (!validator.isValid()) {
+  // validator.disableSubmitButtons(true);
+  // }
+
+  $('[name^=radius]', '.antares-form').on('change', function(e) {
+    var form = $(this).parents('form');
+    form.data('bootstrapValidator').updateStatus('radius', 'NOT_VALIDATED').validateField('radius');
+  });
