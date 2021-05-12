2e91ce4d2edd8ca0329e799dcaeed8dbee0a8f39
Denys Savchenko <speleoden@gmail.com>
2021-05-12 10:05:58 +0200

    fix jq

--- instruments/astrooda_antares/astrooda_antares.js
+++ instruments/astrooda_antares/astrooda_antares.js
@@ -1,37 +1,38 @@
-var avalidator = $('.antares-form').bootstrapValidator({
-    // live :'disabled',
-    fields: {
-      'radius': {
-        // enabled: false,
-        validators: {
-          callback: {
-            callback: function(value, avalidator, $field) {
-              var arad = avalidator.getFieldElements('radius').val();
-              if ( Number(arad) < 0.1 || Number(arad) > 2.5 ) {
-                return {
-                  valid: false,
-                  message: 'Please enter value between 0.1 and 2.5 deg'
+$(document).ready(function(){
+    $('.antares-form').bootstrapValidator({
+        // live :'disabled',
+        fields: {
+            'radius': {
+                // enabled: false,
+                validators: {
+                    callback: {
+                        callback: function(value, validator, $field) {
+                            var arad = validator.getFieldElements('radius').val();
+                            if ( Number(arad) < 0.1 || Number(arad) > 2.5 ) {
+                                return {
+                                valid: false,
+                                message: 'Please enter value between 0.1 and 2.5 deg'
+                                };
+                            }
+                        return true;
+                        }   
+                    }
                 }
-              }
-              return true;
-            }
-          }
+            },
+        },
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
         }
-      },
+    }).data('bootstrapValidator');// .validate();
 
-    },
-    feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',
-      invalid: 'glyphicon glyphicon-remove',
-      validating: 'glyphicon glyphicon-refresh'
-    }
-  }).data('bootstrapValidator');// .validate();
+    // if (!validator.isValid()) {
+    // validator.disableSubmitButtons(true);
+    // }
 
-  // if (!validator.isValid()) {
-  // validator.disableSubmitButtons(true);
-  // }
-
-  $('[name^=radius]', '.antares-form').on('change', function(e) {
-    var form = $(this).parents('form');
-    form.data('bootstrapValidator').updateStatus('radius', 'NOT_VALIDATED').validateField('radius');
-  });
+    $('[name^=radius]', '.antares-form').on('change', function(e) {
+        var form = $(this).parents('form');
+        form.data('bootstrapValidator').updateStatus('radius', 'NOT_VALIDATED').validateField('radius');
+    });
+})
